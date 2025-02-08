e811390fc550fc82894882faf9eb760a5c87923e
Lockhart <jsl5865@psu.edu>
2025-02-08 14:22:59 -0500

    Vessel Diameter Time Lag

--- MDF File Postprocessing/VesselDiameter_TimeLag.m
+++ MDF File Postprocessing/VesselDiameter_TimeLag.m
@@ -3,81 +3,95 @@
 %       Determine the lag-time between two cross-section of a penetrating
 %       arteriole's trunk.
 %   Notes:
-%       This script takes two sets of inputs. The first set [a,b] is the 
-%       frame where a vasodilation event begins and finishes. The second
-%       set are two .txt files contianing the vessel diameters obtained
-%       from the Transform in Radon Space (TiRS) function.
-%       This is a proof of concept and only works for a single vasodilaiton
-%       event.
+%       This script accepts two .txt files, each containing penetrating 
+%       arteriole diameters at a specific frame. In the 'Parameters'
+%       section, identify the frames at which vasodilation events begin, as
+%       well as the duration of the event, the median filter level, and the
+%       frame rate of the resonant two-photon microscope at the time of
+%       recording. 
+%       The .txt files can be obtained from the Transform in Radon Space
+%       (TiRS) function.
+%       This will become a part of the pipeline to analyze data, but must
+%       first be refined to accept ROIs of variable length, converted to a
+%       function, and ...
 %   Output:
-%       Figure 1 - Raw penetrating arteriole diameter plotted for both .txt
-%                  files and overlayed. Followed by a median filter to the
-%                  data, and then normalized with respect to baseline
-%                  diameter (overal mean of data).
-%       Figure 2 - Diameter of penetrating arteriole at both cross-sections
-%                  plotted with region of interest based on [a,b].
-%       Figure 3 - Normalized penetrating arteriole diameter for both
-%                  cross-sections with region of interest identified.
-%                  Normalized plot zoomed into region of interest. Cross
-%                  correlation of the two data sets.
-clear; clc; format short; format compact;
-%% Data
+%       Figure 1 - 3x3 window of plots. Columns 1,2,3 represent the raw 
+%                  penetrating arteriole diameter, filtered penetrating 
+%                  arteriole diameter, and normalized penetrating areriole 
+%                  diameter, respectively. Rows 1,2,3 represent the upper 
+%                  cross-section, lower cross-section, and overlayed 
+%                  cross-sections of the penetrating arteriole trunk.
+%       Figure 2 - Depicts the normalzied penetrating arteriole diameter of
+%                  both cross-sections along with highlighted regions of
+%                  interest. Following are cropped plots displaying only
+%                  regions of interests, and finaly the cross correlation
+%                  for each region of interest.
 
-a = 17650; b = 17800;
+%% Parameters
+Parameter.FilterSize = 5;
+Parameter.Gap = 100;
+Parameter.FPS = 39.55;
+Parameter.ROIs = [600; 2900; 7300; 7750; 12050; 12600; 16850];
+    Parameter.ROIs(:,2) = Parameter.ROIs(:,1) + Parameter.Gap;
+Parameter.CorrelationCoefficient = []; 
+Parameter.Lag = [];
 
+%% Data - General Data Processing
 Data.Top.RawArea = readtable("JSL000_Practice6_241204_Processing_Top_Area.txt");
 Data.Top.Time = length(Data.Top.RawArea.area)/39.55;
 Data.Top.Area = Data.Top.RawArea.area;
 Data.Top.Diameter = 2*sqrt(Data.Top.Area/pi);
-Data.Top.FilteredDiameter = medfilt3(Data.Top.Diameter);
+Data.Top.FilteredDiameter = medfilt1(Data.Top.Diameter,Parameter.FilterSize);
 Data.Top.Normalized = 100*(Data.Top.FilteredDiameter - mean(Data.Top.FilteredDiameter))/mean(Data.Top.FilteredDiameter);
-Data.Top.ForCorrelation = Data.Top.Normalized(a:b) - mean(Data.Top.Normalized(a:b));
 
 Data.Bottom.RawArea = readtable("JSL000_Practice6_241204_Processing_Bottom_Area.txt");
 Data.Bottom.Time = length(Data.Bottom.RawArea.area)/39.55;
 Data.Bottom.Area = Data.Bottom.RawArea.area;
 Data.Bottom.Diameter = 2*sqrt(Data.Bottom.Area/pi);
-Data.Bottom.FilteredDiameter = medfilt3(Data.Bottom.Diameter);
+Data.Bottom.FilteredDiameter = medfilt1(Data.Bottom.Diameter,Parameter.FilterSize);
 Data.Bottom.Normalized = 100*(Data.Bottom.FilteredDiameter - mean(Data.Bottom.FilteredDiameter))/mean(Data.Bottom.FilteredDiameter);
-Data.Bottom.ForCorrelation = Data.Bottom.Normalized(a:b) - mean(Data.Bottom.Normalized(a:b));
 
-Data.Range.Top = length(Data.Top.RawArea.area);
-Data.Range.Bottom = length(Data.Bottom.RawArea.area);
-Data.Range.Range = min(Data.Range.Top, Data.Range.Bottom);
+%% Data - ROI & Correlations
+for i = 1:size(Parameter.ROIs,1)
+    TopBaseline= Data.Top.Normalized(Parameter.ROIs(i,1):Parameter.ROIs(i,2)) - mean(Data.Top.Normalized(Parameter.ROIs(i,1):Parameter.ROIs(i,2)));
+    BottomBaseline= Data.Bottom.Normalized(Parameter.ROIs(i,1):Parameter.ROIs(i,2)) - mean(Data.Bottom.Normalized(Parameter.ROIs(i,1):Parameter.ROIs(i,2)));
+    [CC,L] = xcorr(BottomBaseline,TopBaseline,"normalized");
+    Parameter.CorrelationCoefficient(:,i) = CC;
+    Parameter.Lag(:,i) = L';
+    Parameter.ColorMap(i,:) = randi([0,10],[1,3])/10;
+end
 
-[CorrelationCoefficient1, Lags1] = xcorr(Data.Bottom.ForCorrelation, Data.Top.ForCorrelation);
-
-
-%% Plot
-
+%% Plot - Processing
+TimeTop = (1:height(Data.Top.RawArea))/Parameter.FPS;
+TimeBottom = (1:height(Data.Bottom.RawArea))/Parameter.FPS;
 figure(1)
 t = tiledlayout(1,3);
     t1 = tiledlayout(t,3,1);
         t1.Layout.Tile = 1;
         title(t1,"Raw P.A. Area (Running)");
         xlabel(t1,'Time [s]')
-        nexttile(t1);   plot(Data.Top.RawArea.area); hold on;
+        nexttile(t1);   plot(TimeTop,Data.Top.RawArea.area); hold on;
                         title('P.A. Upper Layer (20\mum deep)')
                         ylabel('P.A. Area [\mum^2]');
-        nexttile(t1);   plot(Data.Bottom.RawArea.area,'k');
+        nexttile(t1);   plot(TimeBottom,Data.Bottom.RawArea.area,'k');
                         title('P.A. Lower Layer (70\mum deep)')
                         ylabel('P.A. Area [\mum^2]');
-        nexttile(t1);   plot(Data.Top.RawArea.area); hold on;
-                        plot(Data.Bottom.RawArea.area,'k'); hold on;
+        nexttile(t1);   plot(TimeTop,Data.Top.RawArea.area); hold on;
+                        plot(TimeBottom,Data.Bottom.RawArea.area,'k'); hold on;
                         title('P.A. Upper & Lower Layer (20\mum, 70\mum deep)');
                         legend('20\mum','70\mum')
     t2 = tiledlayout(t,3,1);
         t2.Layout.Tile = 2;
         title(t2,"Filtered P.A. Diameteter (Running)")
         xlabel(t2, 'Time [s]')
-        nexttile(t2);   plot(Data.Top.FilteredDiameter);
+        nexttile(t2);   plot(TimeTop,Data.Top.FilteredDiameter);
                         title('P.A. Upper Layer (20\mum deep)')
                         ylabel('P.A. Diameter [\mum]');
-        nexttile(t2);   plot(Data.Bottom.FilteredDiameter,'k');
+        nexttile(t2);   plot(TimeBottom,Data.Bottom.FilteredDiameter,'k');
                         title('P.A. Lower Layer (70\mum deep)')
                         ylabel('P.A. Diameter [\mum]');
-        nexttile(t2);   plot(Data.Top.FilteredDiameter); hold on;
-                        plot(Data.Bottom.FilteredDiameter,'k'); hold on;
+        nexttile(t2);   plot(TimeTop,Data.Top.FilteredDiameter); hold on;
+                        plot(TimeBottom,Data.Bottom.FilteredDiameter,'k'); hold on;
                         title('P.A. Upper & Lower Layer (20\mum, 70\mum deep)');
                         ylabel('P.A. Diameter [\mum]');
                         legend('20\mum','70\mum')
@@ -85,62 +99,64 @@ t = tiledlayout(1,3);
         t3.Layout.Tile = 3;
         title(t3,"Normalized P.A. Diameteter (Running)")
         xlabel(t3, 'Time [s]')
-        nexttile(t3);   plot(Data.Top.Normalized);
+        nexttile(t3);   plot(TimeTop,Data.Top.Normalized);
                         title('P.A. Upper Layer (20\mum deep)')
                         ylabel('P.A. \DeltaDiameter/Diameter [%]');
-        nexttile(t3);   plot(Data.Bottom.Normalized,'k');
+        nexttile(t3);   plot(TimeBottom,Data.Bottom.Normalized,'k');
                         title('P.A. Lower Layer (70\mum deep)')
                         ylabel('P.A. \DeltaDiameter/Diameter [%]');
-        nexttile(t3);   plot(Data.Top.Normalized); hold on;
-                        plot(Data.Bottom.Normalized,'k'); hold on;
+        nexttile(t3);   plot(TimeTop,Data.Top.Normalized); hold on;
+                        plot(TimeBottom,Data.Bottom.Normalized,'k'); hold on;
                         title('P.A. Upper & Lower Layer (20\mum, 70\mum deep)');
                         ylabel('P.A. \DeltaDiameter/Diameter [%]');
                         legend('20\mum','70\mum')
+%% Plot - Correlation
 figure(2)
 t = tiledlayout(3,1);
     t1 = tiledlayout(t,1,1);
-        t1.Layout.Tile = 1;
-        title('P.A. Upper & Lower Layer (20\mum, 70\mum deep)');
-        ylabel('P.A. \DeltaDiameter/Diameter [%]');
-            plot(Data.Top.Normalized); hold on;
-            plot(Data.Bottom.Normalized,'k'); hold on;
-            title('P.A. Upper & Lower Layer (20\mum, 70\mum deep)');
-            ylabel('P.A. \DeltaDiameter/Diameter [%]');
-            legend('20\mum','70\mum')
-            xline(a,'--r',LineWidth=2); xline(b,'--r',LineWidth=2);
-
-
-
-figure(3)
-tiledlayout(3,1);
-nexttile;
+    t1.Layout.Tile = 1;
+    title(t,'Cross Correlation of JSL000.6 Penetrating Arteriole while Running');
+    xlabel(t,'Time [s]');
+        nexttile(t1);
+        plot(TimeTop,Data.Top.Normalized); hold on;
+        plot(TimeBottom,Data.Bottom.Normalized,'k'); hold on;
+        title(t1,'P.A. Upper & Lower Layer (20\mum, 70\mum deep)')
+        ylabel('P.A. \DeltaD/D [%]');
+        legend('20\mum','70\mum')
+        for i = 1:size(Parameter.ROIs,1)
+            xline(TimeTop(Parameter.ROIs(i,1)),'--r',LineWidth=2,HandleVisibility='off',Color=Parameter.ColorMap(i,:)); 
+            xline(TimeBottom(Parameter.ROIs(i,2)),'--r',LineWidth=2,HandleVisibility='off',Color=Parameter.ColorMap(i,:));
+            axis('tight')
+        end
+    t2 = tiledlayout(t,1,size(Parameter.ROIs,1));
+    t2.Layout.Tile = 2;
+        title(t2,'P.A. Upper & Lower Layer (20\mum, 70\mum deep) Sections');
+        ylabel(t2,'P.A. \DeltaD/D [%]');
+        for i = 1:size(Parameter.ROIs,1)
+            nexttile(t2);
+            plot(TimeTop(Parameter.ROIs(i,1):Parameter.ROIs(i,2)),Data.Top.Normalized(Parameter.ROIs(i,1):Parameter.ROIs(i,2))); hold on;
+            plot(TimeBottom(Parameter.ROIs(i,1):Parameter.ROIs(i,2)),Data.Bottom.Normalized(Parameter.ROIs(i,1):Parameter.ROIs(i,2)),'k'); hold on;
+            xline(TimeTop(Parameter.ROIs(i,1)),'--r',LineWidth=2,HandleVisibility='off',Color=Parameter.ColorMap(i,:)); 
+            xline(TimeTop(Parameter.ROIs(i,2)),'--r',LineWidth=2,HandleVisibility='off',Color=Parameter.ColorMap(i,:));
+            axis('tight')
+        end
+    t3 = tiledlayout(t,1,size(Parameter.ROIs,1));
+    t3.Layout.Tile = 3;
+        title(t3,'Cross Correlation of P.A. Parameter.ROIs');
+        ylabel(t3,'Cross Correlation Coefficient')
+        for i = 1:size(Parameter.ROIs,1)
+            nexttile(t3)
+            xlim([-Parameter.Gap,Parameter.Gap])
+            plot(Parameter.Lag(:,i),Parameter.CorrelationCoefficient(:,i)); hold on;
+            xline(0,'--k');
+            xline(-Parameter.Gap,'--r',LineWidth=2,HandleVisibility='off',Color=Parameter.ColorMap(i,:)); 
+            xline(Parameter.Gap,'--r',LineWidth=2,HandleVisibility='off',Color=Parameter.ColorMap(i,:));
+            axis('tight')
+        end
+%%
+figure(4)
 plot(Data.Top.Normalized); hold on;
-plot(Data.Bottom.Normalized,'k'); hold on;
-title('P.A. Upper & Lower Layer (20\mum, 70\mum deep)');
-ylabel('P.A. \DeltaDiameter/Diameter [%]');
-legend('20\mum','70\mum')
-xline(a,'--r',LineWidth=2); xline(b,'--r',LineWidth=2);
-nexttile
-plot(Data.Top.Normalized(a:b)); hold on;
-plot(Data.Bottom.Normalized(a:b),'k');
-xlim([-5,b-a+5]);
-xline(0,'--r',LineWidth=2); xline(b-a,'--r',LineWidth=2);
-nexttile
-plot(Lags1,CorrelationCoefficient1); hold on;
-xlim([-(b-a+5),b-a+5]);
-xline(0)
-xline(-(b-a),'--r',LineWidth=2); xline(b-a,'--r',LineWidth=2);
-xline(157-.5*length(CorrelationCoefficient1))
-% 
-% %%
-% [c,lags] = xcorr(Area.Data.Bottom(7200:7700)-mean(Area.Data.Bottom(7200:7700)),Area.Data.Top(7250:7400)-mean(Area.Data.Top(7250:7400)));
-% 
-% figure('Name','xcorr')
-% plot(lags,c); hold on;
-% xline(0)
-% %%
-% figure('Name','raw')
-% plot(Area.Data.Top(7250:7400))
-% %%
-% figure('Name','raw2')
-% plot(Area.Data.Bottom(7250:7400))
+plot(Data.Bottom.Normalized); hold on;
+axis('tight')
+xregion(length(Data.Top.Normalized),0,'FaceColor','b')
+x_bin = Data.Top.Normalized > Data.Bottom.Normalized(1:end-1,1);
