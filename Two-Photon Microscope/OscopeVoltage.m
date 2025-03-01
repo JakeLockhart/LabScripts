c27dbd09f96af72d1de0cb1cfa2ba2d2d3d41618
Lockhart <jsl5865@psu.edu>
2025-03-01 16:15:45 -0500

    Plots finished. One potential update: dynamically determine pulse width & gap for xlim()

--- Two-Photon Microscope/OscopeVoltage.m
+++ Two-Photon Microscope/OscopeVoltage.m
@@ -57,11 +57,48 @@ for i = 1:SystemProperties.FilePath.Data.Length
     HighSignal.Voltage(i,:) = HighSignal.BinCenter(i, HighLocation(Index));
 end
 
-%% Plot Global Histogram and Single Steps
-t = tiledlayout(1,2);
-set(gcf,"Color", [0 0 0])
+%% Plot Initial Signal Processing
+figure(1)
+t = tiledlayout(3,1);
+title(t, "MScan Oscilloscope Data Processing", 'Color', 'white')
+ylabel(t, "Waveform Voltage [mV]", 'Color', 'white');
 ColorMap = hsv(SystemProperties.FilePath.Data.Length);
+set(gcf,"Color", [0 0 0])
+nexttile(1)
+title("Raw Signal Data", 'Color', 'white')
+axis tight;
+set(gca, 'Color', [0 0 0]); 
+set(gca, 'XColor', 'white', 'YColor', 'white');
+hold on;
+nexttile(2)
+title("Aligned Signal Data", 'Color', 'white')
+axis tight;
+set(gca, 'Color', [0 0 0]); 
+set(gca, 'XColor', 'white', 'YColor', 'white');
+hold on;
+nexttile(3)    
+title("Aligned Single Step Waveforms", 'Color', 'white')
+xlim([-25, 150]); ylim([1.05*min(min(Oscope.AlignedVoltage)), 1.1*max(max(Oscope.AlignedVoltage))]);
+xlabel("Time [\mus]", 'Color', 'white');
+set(gca, 'Color', [0 0 0]); hold on;
+set(gca, 'XColor', 'white', 'YColor', 'white');
+
+for i = 1:SystemProperties.FilePath.Data.Length
+    nexttile(1)
+    plot(Oscope.Time(i,:), Oscope.Voltage(i,:), "Color", ColorMap(i,:)); hold on;
+    nexttile(2)
+    plot(Oscope.Time(i,:), Oscope.AlignedVoltage(i,:), "Color", ColorMap(i,:)); hold on;
+    nexttile(3)
+    plot(Oscope.Time(i,:),Oscope.AlignedVoltage(i,:), "Color", ColorMap(i,:)); hold on;
+    pause(0.01)
+end
+
+%% Plot Global Histogram and Single Steps
+figure(2)
+t = tiledlayout(1,3);
 title(t, "Voltage Step & Histogram", 'Color', 'white');
+ColorMap = hsv(SystemProperties.FilePath.Data.Length);
+set(gcf,"Color", [0 0 0])
 nexttile(1)
 title("Low & High Signal Histogram for Each Waveform", 'Color', 'white')
 xlabel("Waveform Voltage [mV]", 'Color', 'white'); ylabel("Frequency [Counts]", 'Color', 'white');
@@ -76,86 +113,54 @@ xlabel("Time [\mus]", 'Color', 'white'); ylabel("Voltage [mV]", 'Color', 'white'
 xlim([-25, 150]); ylim([1.05*min(min(Oscope.AlignedVoltage)), 1.1*max(max(Oscope.AlignedVoltage))]);
 set(gca, 'Color', [0 0 0]); hold on;
 set(gca, 'XColor', 'white', 'YColor', 'white');
+nexttile(3)
+title("High & Low Signal Voltages", 'Color', 'white')
+xlabel("Laser Input Intensity", 'Color', 'white'); ylabel("Voltage [mV]", 'Color', 'white');
+set(gca, 'Color', [0 0 0]); hold on;
+set(gca, 'XColor', 'white', 'YColor', 'white');
+Interval = [-1,0:5:100];
+grid on; axis tight;
 for i = 1:SystemProperties.FilePath.Data.Length
     nexttile(1)
     bar(WF_T.BinCenter(i,:), WF_T.Count(i,:), "FaceAlpha", 0.75, "FaceColor", ColorMap(i,:), "EdgeColor", "none"); hold on;
     nexttile(2)
     plot(Oscope.Time(i,:), Oscope.AlignedVoltage(i,:), "Color", ColorMap(i,:)); hold on;
+    nexttile(3)
+    plot(Interval(i), LowSignal.Voltage(i), '.', 'MarkerSize', 30, 'Color', ColorMap(i,:), 'HandleVisibility', 'off'); hold on;
+    plot(Interval(i), HighSignal.Voltage(i), '.', 'MarkerSize', 30, 'Color', ColorMap(i,:), 'HandleVisibility', 'off'); hold on;
     pause(0.5);
 end
+    plot(Interval, HighSignal.Voltage, 'w--', 'HandleVisibility', 'off'); hold on;
+    plot(Interval, LowSignal.Voltage, 'w--', 'HandleVisibility', 'off'); hold on;
+    plot(Interval, HighSignal.Voltage, '.', 'MarkerSize', 30, 'Color', 'red'); hold on;
+    plot(Interval, LowSignal.Voltage, '.', 'MarkerSize', 30, 'Color', 'blue'); hold on;
+    legend("High Signal Voltage", "Low Signal Voltage", 'Color', 'white', 'Location', 'northwest')
+%% Plot Individual Waveform Histograms
+figure(3)
+t = tiledlayout('flow');
+title(t, "Low & High Signal Histogram for Each Waveform", 'Color', 'white')
+xlabel(t, "Waveform Voltage [mV]", 'Color', 'white'); ylabel(t, "Frequency [Counts]", 'Color', 'white');
+set(gcf,"Color", [0 0 0])
+hold on;
+for i = 1:SystemProperties.FilePath.Data.Length
+    nexttile
+    bar(WF_S.BinCenter(i,:), WF_S.Count(i,:), 'FaceColor', ColorMap(i,:), "EdgeColor", "none"); 
+    xline(LowSignal.Voltage(i,:), 'w--');
+    xline(HighSignal.Voltage(i,:), 'w--');
+    if i == 1
+        Name = "Waveform: Laser off";
+    else
+        Name = "Waveform: " + num2str((i-2)*5) + "%";
+    end
+    title(Name, 'Color', 'white');
+    set(gca, 'Color', [0 0 0]); 
+    set(gca, 'XColor', 'white', 'YColor', 'white');
+    pause(0.05)
+end
 
-%% Single Histogram
-%figure()
-%TotalBins = linspace(0, max(1.25*Oscope.Voltage(end)), 22*10);
-%for i = 1:SystemProperties.FilePath.Data.Length
-%    [Count, BinEdge] = histcounts(Oscope.AlignedVoltage(i,:), TotalBins);
-%    BinCenter = (BinEdge(1:end-1) + BinEdge(2:end))/ 2;
-%
-%    bar(BinCenter, Count, "FaceAlpha", 0.75); hold on;
-%    pause(.1)
-%end
-%
-%%% Histogram to Determine Noise & Amplitudes
-%figure()
-%t = tiledlayout('flow');
-%title(t,"Oscilloscope Waveform Histograms")
-%xlabel(t, "Voltage [mV]"); ylabel(t, "Frequency");
-%for i = 1: SystemProperties.FilePath.Data.Length
-%    TotalBins = sqrt(size(Oscope.AlignedVoltage(i,:),2));
-%    [Count, BinEdge] = histcounts(Oscope.AlignedVoltage(i,:), TotalBins);
-%    BinCenter = (BinEdge(1:end-1) + BinEdge(2:end))/ 2;
-%
-%    LowSignal.Count = Count(1:round(end/2));
-%    LowSignal.BinEdge = BinEdge(1:round(end/2));
-%    LowSignal.BinCenter = BinCenter(1:round(end/2));
-%    [LowPeak, LowLocation] = findpeaks(LowSignal.Count);
-%    [~, Index] = max(LowPeak);
-%    LowSignal.Voltage(i) = LowSignal.BinCenter(LowLocation(Index));
-%
-%    HighSignal.Count = Count(round(end/2):end);
-%    HighSignal.BinEdge = BinEdge(round(end/2):end);
-%    HighSignal.BinCenter = BinCenter(round(end/2):end);
-%    [HighPeak, HighLocation] = findpeaks(HighSignal.Count);
-%    [~, Index] = max(HighPeak);
-%    HighSignal.Voltage(i) = HighSignal.BinCenter(HighLocation(Index));
-%
-%    nexttile
-%    bar(BinCenter,Count); hold on;
-%    xline(LowSignal.Voltage(i), "Color", 'b'); hold on;
-%    xline(HighSignal.Voltage(i), "Color", 'r'); hold on;
-%    if i == 1
-%        Name = "Waveform: Laser off";
-%    else
-%        Name = "Waveform: " + num2str((i-2)*5) + "%";
-%    end
-%    title(Name);
-%end
-%
-%%% Plot Waveforms
-%figure()
-%t = tiledlayout("flow");
-%title(t, "MScan Output Voltage Measurement from 2P Oscilloscope");
-%xlabel(t, "Time (\mus)");  ylabel(t, "Voltage (mV)"); 
-%nexttile    %   --- Raw Data ---
-%for i = 1:SystemProperties.FilePath.Data.Length
-%    plot(Oscope.Time(i,:),Oscope.Voltage(i,:));
-%    hold on;
-%end
-%title("Raw Voltage Output"); axis tight;
-%nexttile    %   --- Sync Waveform Periods --
-%for i = 1:SystemProperties.FilePath.Data.Length
-%    plot(Oscope.Time(i,:), Oscope.AlignedVoltage(i,:));
-%    hold on;
-%end
-%title("Aligned Voltage Output"); axis tight;
-%nexttile    %   --- Show a Single Step ---
-%for i = 1:SystemProperties.FilePath.Data.Length
-%    plot(Oscope.Time(i,:),Oscope.AlignedVoltage(i,:));
-%    hold on;
-%end
-%title("Synced Single Step")
-%xlim([-50, 150]);
-%nexttile    %   --- Histogram Results (Plot) ---
+%% Plot Results
+
+
 %Interval = [-1,0:5:100];
 %plot(Interval, LowSignal.Voltage); hold on;
 %plot(Interval, HighSignal.Voltage); hold on;
