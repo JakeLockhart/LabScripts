10269fcd2bdb8c9039df7040c7dc3776472c3642
Lockhart <jsl5865@psu.edu>
2025-03-17 09:45:19 -0400

    Added README & began Arduino Due output validation scripts

--- Two-Photon Microscope/OscilloscopeMeasurement/OscopeCircuitAnalysis.m
+++ Two-Photon Microscope/OscilloscopeMeasurement/OscopeCircuitAnalysis.m
@@ -24,3 +24,47 @@ for i = 1:SystemProperties.FilePath.Data.Length
     Oscope.Time(i,:) = TempFile{:,4}/Oscope.SampleInterval(i);
     Oscope.Voltage(i,:) = TempFile{:,5};
 end
+
+%% Plot Voltage/Time
+for i = 1:SystemProperties.FilePath.Data.Length
+    figure(i);
+    plot(Oscope.Time(i,:), Oscope.Voltage(i,:));
+    axis tight;
+    hold on;
+end
+
+figure();
+t = tiledlayout(4,1);
+title(t, "Oscope Circuit Amplitude Validation", 'Color', 'white');
+ColorMap = hsv(SystemProperties.FilePath.Data.Length);
+set(gcf, "Color", [0 0 0]);
+nexttile(1);
+title("Simulated MScan Input Signals");
+xlabel("Time [\mus]", 'Color', 'white'); ylabel("Voltage [V]", 'Color', 'white');
+hold on;
+nexttile(2);
+title("TTL Pulses Output to ETL");
+xlabel("Time [\mus]", 'Color', 'white'); ylabel("Voltage [V]", 'Color', 'white');
+hold on;
+nexttile(3);
+title("Analog Pulses Output to Amplifier (302RM)");
+xlabel("Time [\mus]", 'Color', 'white'); ylabel("Voltage [V]", 'Color', 'white');
+hold on;
+nexttile(4);
+title("Overlayed Signals", 'Color', 'white')
+xlabel("Time [\mus]", 'Color', 'white'); ylabel("Voltage [V]", 'Color', 'white');
+hold on;
+
+for i = 1:SystemProperties.FilePath.Data.Length
+    nexttile(i);
+    plot(Oscope.Time(i,:), Oscope.Voltage(i,:), "Color", ColorMap(i,:));
+    set(gca, 'Color', [0 0 0]);  set(gca, 'XColor', 'white', 'YColor', 'white');
+    axis tight; hold on;
+end
+nexttile(4);
+for i = 1:SystemProperties.FilePath.Data.Length
+    plot(Oscope.Time(i,:), Oscope.Voltage(i,:), "Color", ColorMap(i,:));
+    set(gca, 'Color', [0 0 0]);  set(gca, 'XColor', 'white', 'YColor', 'white');
+    axis tight; hold on;
+    xlim([-250,250]);
+end
