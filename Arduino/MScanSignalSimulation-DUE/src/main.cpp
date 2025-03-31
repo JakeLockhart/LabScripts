edbda8634d116ff969d3ed4ba3c088f4d25abfd8
Lockhart <jsl5865@psu.edu>
2025-03-31 15:56:06 -0400

    Updated to include both an analog output (DAC0) and digital output (Digital7) to test limitations of Giga's attachInterrupt() function

--- Arduino/MScanSignalSimulation-DUE/src/main.cpp
+++ Arduino/MScanSignalSimulation-DUE/src/main.cpp
@@ -2,31 +2,36 @@
 
 // Arduino Due Board Pins
 int Potentiometer = A0;
-int MScanOutput = DAC0;
+int Analog_Output = DAC0;
+int Digital_Output = 7;
 
 // Parameters
-int TTL_Amplitude = 700;    // Desired amplitude [mV]
+int TTL_Amplitude = 0;    // Desired amplitude [mV]
 int PPAdjust = 100;         // Fine tuning range [±50mV]
 int PulseWidth = 100;       // Pulse width duration [um]
 int PulseGap = 100;         // Pulse gap duration [um]
-int TuneRange = 600;        // Fine-tuning range for potentiometer [±100mV]
+int TuneRange = 1000;        // Fine-tuning range for potentiometer [±100mV]
 int Amplitude_Bits = map(TTL_Amplitude, 550, 3300, 0, 4095);    // Convert Amplitude from mV to DAC (0.5-3.3V = 0-4095)
 
 
 void setup() {
-    pinMode(MScanOutput, OUTPUT);   // Setup pins
+    pinMode(Analog_Output, OUTPUT);   // Setup pins
     analogWriteResolution(12);      // Read/Write resolution is 12-bits
     pinMode(Potentiometer, INPUT);
     analogReadResolution(12);    
+    pinMode(Digital_Output, OUTPUT);
 }
 
 void loop() {
     int PValue = analogRead(Potentiometer);                                         // Read potentiometer analog input value
     int TuneBits = map(PValue, 0, 4095, -TuneRange, TuneRange) * 4095 / (3300-550); // Map analog input to bit range and convert to mV range
-    int MScanOutput_Amplitude = constrain(Amplitude_Bits + TuneBits, 0, 4095);      // Create boundaries so that analog input does not exceed DAC range
+    int Analog_Output_Amplitude = constrain(Amplitude_Bits + TuneBits, 0, 4095);      // Create boundaries so that analog input does not exceed DAC range
 
-    analogWrite(MScanOutput, MScanOutput_Amplitude);    // Create TTL pulse
+    analogWrite(Analog_Output, Analog_Output_Amplitude);    // Create analog pulse
+    digitalWrite(Digital_Output, HIGH);
     delayMicroseconds(PulseWidth);
-    analogWrite(MScanOutput, 0);
+    analogWrite(Analog_Output, 0);
+    digitalWrite(Digital_Output, LOW);
     delayMicroseconds(PulseGap);
+    
 }
