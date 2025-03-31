36c90dd6e4c0ab4a9af34b5464b4c1188eb54280
Lockhart <jsl5865@psu.edu>
2025-03-31 15:54:44 -0400

    Created test script to troubleshoot Arduino Giga. Currently used to test why Serial Monitor is not working. Reason: More complicated boards require an initialization phase to start the serial monitor. The Giga is one of these boards. By adding 'while(!Serial){};' after 'Serial.begin(9600)' the void setup (){} is paused until the serial monitor is active. Next troubleshoot will be the attachInterrupt() function which is either crashing the board or not responding to incoming signals.

--- Arduino/GIGAtest/src/main.cpp
+++ Arduino/GIGAtest/src/main.cpp
@@ -4,18 +4,24 @@
 
 int pulseWidth = 100;
 int pulseGap = 200;
-int values[] = {100, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000}; 
+// int values[] = {100, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000}; 
+int values[] = {2000};
 
 void setup() {
   pinMode(DAC0, OUTPUT);
   analogWriteResolution(12); 
+  Serial.begin(9600);
+  Serial.print(values[1]);
 }
 
 void loop() {
   for (int i = 0; i < sizeof(values) / sizeof(values[0]); i++) {
     analogWrite(DAC0, values[i]);
-    delay(pulseWidth);
+    delayMicroseconds(pulseWidth);
     analogWrite(DAC0, 0);
-    delay(pulseGap);
+    delayMicroseconds(pulseGap);
   }
+  Serial.println("Hello World");
+  delay(10);
+  Serial.println(values[1]);
 }
