f7a23d433788f530ef377b6252eefd498740dd8e
Lockhart <jsl5865@psu.edu>
2025-04-05 17:20:24 -0400

    Added error check to ensure that FileType is string ('csv', 'txt', etc.). Added an error message if user cancels operation

--- Functions/FileLookup.m
+++ Functions/FileLookup.m
@@ -1,9 +1,12 @@
 function Lookup = FileLookup(FileType)
-    if ~ischar(FileType) && ~isstring(FileType)
-        error('File type must be a string or character array.');
-    end
-    Lookup.FileType = strcat('*.', FileType);                                       % Choose file type
+    if ~ischar(FileType) && ~isstring(FileType)                                         % Ensure input is a string ('csv', 'txt', etc)
+        error('File type must be a string or character array.');                        %   Throw error message otherwise
+    end                                                                                 %   Continue
+    Lookup.FileType = strcat('*.', FileType);                                           % Choose file type
     Lookup.FolderAddress = uigetdir('*.*','Select a file');                             % Choose folder path location
+    if isequal(Lookup.FolderAddress, 0)                                                 % Esure directory is found and user did not cancel operation
+        error('No File Selected');                                                      %   Throw error message otherwise
+    end                                                                                 %   Continue
     Lookup.AllFiles = Lookup.FolderAddress + "\" + Lookup.FileType;                     % Convert to filepath
     Lookup.FolderAddress = erase(Lookup.AllFiles, Lookup.FileType);                     % Create beginning address for file path
     [Lookup.CurrentFolder, ~, ~] = fileparts(Lookup.AllFiles);                          % Collect folder information
