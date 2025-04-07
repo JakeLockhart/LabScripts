d33decf25b3d831340579167dcefeb31e5040b4f
Lockhart <jsl5865@psu.edu>
2025-04-07 13:29:09 -0400

    This method works!! The created pulses need to have a pulse width less than the pulse width of the incoming interrupt signals. Validation needed, but this method seems correct.

--- Arduino/ETLandEOM_Controller-GIGA/src/Parameters.cpp
+++ Arduino/ETLandEOM_Controller-GIGA/src/Parameters.cpp
@@ -18,7 +18,7 @@ int TTLPulse_ETL = 7;   // Outgoing TTL pulse to Gardasoft, then ETL
 int TTLPulse_EOM = DAC0;   // Outgoing TTL pulse to Amplifier (J3 port) to control EOM (Pockel Cell)
 
 // Declare derivative/constant variables
-int Delay = 100;
+int Delay = 75;
 float ReferenceVoltage = 3.3;
 int CurrentImagingPlane = TotalImagingPlanes;
 volatile bool Flag = false;
