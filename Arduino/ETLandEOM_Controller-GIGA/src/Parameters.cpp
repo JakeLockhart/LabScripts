790fa94c71453f3a09c8d5d61e6728dbcae83bf6
jake <jakelockhart65@gmail.com>
2025-04-08 11:17:25 -0400

    Created README.md anddescribed project.

--- Arduino/ETLandEOM_Controller-GIGA/src/Parameters.cpp
+++ Arduino/ETLandEOM_Controller-GIGA/src/Parameters.cpp
@@ -13,9 +13,9 @@ int InputIntensity[MAX_Planes] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}; // T
 
 //---------------------- Do Not Edit --------------------------------------------------------------
 // Arduino Board Pins (TTL Input/Output)
-int NewFrame_MScan = 2; // Income TTL pulse from MScan that occurs at every new frame
-int TTLPulse_ETL = 7;   // Outgoing TTL pulse to Gardasoft, then ETL 
-int TTLPulse_EOM = DAC0;   // Outgoing TTL pulse to Amplifier (J3 port) to control EOM (Pockel Cell)
+int NewFrame_MScan = 2;     // Income TTL pulse from MScan that occurs at every new frame
+int TTLPulse_ETL = 7;       // Outgoing TTL pulse to Gardasoft, then ETL 
+int TTLPulse_EOM = DAC0;    // Outgoing TTL pulse to Amplifier (J3 port) to control EOM (Pockel Cell)
 
 // Declare derivative/constant variables
 int PulseWidth = 32;
