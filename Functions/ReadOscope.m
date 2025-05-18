423579704e02fbd049eb35352d2ba066117afbde
Lockhart <jsl5865@psu.edu>
2025-05-17 21:29:20 -0400

    Created a function to read all Oscope data based on the Lookup function. This is easier than trying to make these independent of one another.

---
+++ Functions/ReadOscope.m
@@ -0,0 +1,19 @@
+function Oscope = ReadOscope(Lookup)
+    for i = 1:Lookup.FileCount
+        Name = erase(Lookup.FolderInfo(i).name, ".csv");
+        TempFile = readtable(fullfile(Lookup.FolderAddress, Name));
+        Oscope.RecordLength(i) = TempFile{1,2};
+        Oscope.SampleInterval(i) = TempFile{2,2};
+        Oscope.TriggerPoint(i) = TempFile{3,2};
+        Oscope.VerticalScale(i) = TempFile{9,2};
+        Oscope.VerticalOffset(i) = TempFile{10,2};
+        Oscope.HorizontalScale(i) = TempFile{12,2};
+        Oscope.Yzero(i) = TempFile{14,2};
+        Oscope.ProbeAtten(i) = TempFile{15,2};
+        Oscope.TimeInterval_dt(i) =  Oscope.SampleInterval(i)./Oscope.RecordLength(i);
+        Oscope.TotalTime_Tnet(i) =  Oscope.SampleInterval(i).*Oscope.RecordLength(i);
+        timeIndices = linspace(-Oscope.RecordLength(i)/2, Oscope.RecordLength(i)/2, Oscope.RecordLength(i));
+        Oscope.Time(i,:) = 1000 * timeIndices * Oscope.SampleInterval(i);
+        Oscope.Voltage(i,:) = TempFile{:,5};
+    end
+end
