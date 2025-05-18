0fb4639683c69a2f01b06a37a62fe98f7eba9eed
Lockhart <jsl5865@psu.edu>
2025-05-17 21:28:21 -0400

    Updated FileLookup to search within subfolders for files as well as individual folders based on a second argument 'all' vs 'single'

--- Functions/FileLookup.m
+++ Functions/FileLookup.m
@@ -1,16 +1,33 @@
-function Lookup = FileLookup(FileType)
-    if ~ischar(FileType) && ~isstring(FileType)                                         % Ensure input is a string ('csv', 'txt', etc)
-        error('File type must be a string or character array.');                        %   Throw error message otherwise
-    end                                                                                 %   Continue
-    Lookup.FileType = strcat('*.', FileType);                                           % Choose file type
-    Lookup.FolderAddress = uigetdir('*.*','Select a file');                             % Choose folder path location
-    if isequal(Lookup.FolderAddress, 0)                                                 % Esure directory is found and user did not cancel operation
-        error('No File Selected');                                                      %   Throw error message otherwise
-    end                                                                                 %   Continue
-    Lookup.AllFiles = Lookup.FolderAddress + "\" + Lookup.FileType;                     % Convert to filepath
-    Lookup.FolderAddress = erase(Lookup.AllFiles, Lookup.FileType);                     % Create beginning address for file path
-    [Lookup.CurrentFolder, ~, ~] = fileparts(Lookup.AllFiles);                          % Collect folder information
-    Lookup.CurrentFolder = regexp(Lookup.CurrentFolder, '([^\\]+)$', 'match', 'once');  % Determine the parent folder
-    Lookup.FolderInfo = dir(Lookup.AllFiles);                                           % Identify the folder directory
-    Lookup.FileCount = length(Lookup.FolderInfo);                                       % Determine the number of files in folder directory
+function Lookup = FileLookup(FileType, SearchMode)
+    %% Validate file type 
+        if ~ischar(FileType) && ~isstring(FileType)                                 % Ensure input is a string ('csv', 'txt', etc)
+            error('File type must be a string or character array.');                %   Throw error message otherwise
+        end                                                                         %   Continue
+        Lookup.FileType = strcat('*.', FileType);                                   % Choose file type
+    
+    %% Determine search type (Single Folder || All subfolders)
+        if nargin < 2 || isempty(SearchMode)                                        % Assume no subfolders unless otherwise stated
+            SearchMode = 'single';                                                  %   Define file search based on empty argument
+        elseif ~any(strcmpi(SearchMode, {'single', 'all'}))                         %   Validate second argument
+            error("SearchMode must be 'single' or 'all'.");                         %       Throw error message
+        end                                                                         %   Continue
+
+    %% Select the folder
+        Lookup.FolderAddress = uigetdir('*.*','Select a folder');                   % Choose folder path location
+        if isequal(Lookup.FolderAddress, 0)                                         % Esure directory is found and user did not cancel operation
+            error('No folder selected.');                                           %   Throw error message otherwise
+        end                                                                         %   Continue
+
+    %% Determine file search based on SearchMode
+        if strcmpi(SearchMode, 'all')                                               % SearchMode = All files within subfolders
+            searchPattern = fullfile(Lookup.FolderAddress, '**', Lookup.FileType);  %   Find all FileType within subfolders
+        else                                                                        %   SearchMode /= All files within subfolders
+            searchPattern = fullfile(Lookup.FolderAddress, Lookup.FileType);        %   Find all Filetype within single folder
+        end                                                                         %   continue
+
+    %% Find all FileType within defined folder
+        Lookup.AllFiles = searchPattern;                                            % Create general file path
+        Lookup.FolderInfo = dir(searchPattern);                                     % Identify the folder directory
+        Lookup.FileCount = length(Lookup.FolderInfo);                               % Determine the number of files in this folder
+        [~, Lookup.CurrentFolder] = fileparts(Lookup.FolderAddress);                % Collect folder information
 end
