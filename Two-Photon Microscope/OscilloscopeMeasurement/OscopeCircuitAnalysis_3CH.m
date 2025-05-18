b1166ff44a6c521ea24b5e03b71b081f4ae67221
Lockhart <jsl5865@psu.edu>
2025-05-17 21:32:01 -0400

    Updated .CSV read to be based on ReadOscope() function

--- Two-Photon Microscope/OscilloscopeMeasurement/OscopeCircuitAnalysis_3CH.m
+++ Two-Photon Microscope/OscilloscopeMeasurement/OscopeCircuitAnalysis_3CH.m
@@ -3,25 +3,7 @@ clear; clc; format short g; format compact;
 addpath('C:\Workspace\LabScripts\Functions')
 Lookup = FileLookup('csv');
 [LowerBound, UpperBound, Canceled] = UserDefinedPeaks(Lookup, Lookup.FileCount, 'single', 'UseRaw');
-
-%% Read .CSV file
-    for i = 1:Lookup.FileCount
-        Name = erase(Lookup.FolderInfo(i).name, ".csv");
-        TempFile = readtable(fullfile(Lookup.FolderAddress, Name));
-        Oscope.RecordLength(i) = TempFile{1,2};
-        Oscope.SampleInterval(i) = TempFile{2,2};
-        Oscope.TriggerPoint(i) = TempFile{3,2};
-        Oscope.VerticalScale(i) = TempFile{9,2};
-        Oscope.VerticalOffset(i) = TempFile{10,2};
-        Oscope.HorizontalScale(i) = TempFile{12,2};
-        Oscope.Yzero(i) = TempFile{14,2};
-        Oscope.ProbeAtten(i) = TempFile{15,2};
-        Oscope.TimeInterval_dt(i) =  Oscope.SampleInterval(i)./Oscope.RecordLength(i);
-        Oscope.TotalTime_Tnet(i) =  Oscope.SampleInterval(i).*Oscope.RecordLength(i);
-        timeIndices = linspace(-Oscope.RecordLength(i)/2, Oscope.RecordLength(i)/2, Oscope.RecordLength(i));
-        Oscope.Time(i,:) = 1000 * timeIndices * Oscope.SampleInterval(i);
-        Oscope.Voltage(i,:) = TempFile{:,5};
-    end
+Oscope = ReadOscope(Lookup);
 
 %% Cross Correlation
     NormalizedBounded_Input = Oscope.Voltage(1, LowerBound:UpperBound) - mean(Oscope.Voltage(1, LowerBound:UpperBound));
