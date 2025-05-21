ec3f8254482898964af2b1ecf2884f181a84282d
Lockhart <jsl5865@psu.edu>
2025-05-21 11:24:57 -0400

    Removed old createpulse version that made pulses based on etl pulse.

--- Arduino/ETLandEOM_Controller/ETLandEOM_Controller-GIGA/src/CreatePulses.cpp
+++ Arduino/ETLandEOM_Controller/ETLandEOM_Controller-GIGA/src/CreatePulses.cpp
@@ -1,35 +1,6 @@
 #include <Arduino.h>
 #include "Parameters.h"
 
-// Original - ETL pulse causes new voltage step
-//int i = 0;
-//void CreatePulses() {
-//    if (CurrentImagingPlane == 1) {
-//        digitalWrite(TTLPulse_ETL, HIGH);
-//        analogWrite(TTLPulse_EOM, LaserVoltage_Bits[i]);
-//        
-//        delayMicroseconds(PulseWidth);
-//
-//        digitalWrite(TTLPulse_ETL, LOW);
-//        analogWrite(TTLPulse_EOM, 0);
-//        CurrentImagingPlane = TotalImagingPlanes;
-//
-//            if (i == TotalImagingPlanes-1) {
-//                i = 0;
-//            }
-//            else {
-//                i++;
-//            }
-//        }
-//    else {
-//        analogWrite(TTLPulse_EOM, LaserVoltage_Bits[i]);
-//        delayMicroseconds(PulseWidth);
-//        analogWrite(TTLPulse_EOM, 0);
-//        CurrentImagingPlane--;
-//    }
-//}
-
-// New - ETL pulse resets voltage step to i = 0
 int i = 0;
 void CreatePulses() {
     if (CurrentImagingPlane == 1) {
