33b08827892790487e5243f3bbcd018edbd5f6ad
Lockhart <jsl5865@psu.edu>
2025-06-02 12:33:56 -0400

    Updates

--- Two-Photon Microscope/OscilloscopeMeasurement/OscopeMScan_TTLTriggers.m
+++ Two-Photon Microscope/OscilloscopeMeasurement/OscopeMScan_TTLTriggers.m
@@ -60,34 +60,46 @@ t1.Legend.TextColor = "White";
 %% H-Sync and Line Trigger
 figure(2)
 t2.Layout = tiledlayout(4,1);
-t2.NestedLayout = tiledlayout(t2.Layout,1,3);
-t2.NestedLayout.Layout.Tile = 3;
+t2.NestedLayout1 = tiledlayout(t2.Layout,1,3);
+t2.NestedLayout1.Layout.Tile = 3;
 title(t2.Layout, "Line Trigger & H-Sync", "Color", "White");
 set(gcf, "Color", [0, 0, 0]);
 ylabel(t2.Layout, "Voltage [V]", "Color", "White");
 xlabel(t2.Layout, "Time", "Color", "White");
-ColorMap = hsv(2);
-t2.LegendLabels = ["H-Sync (MDR-R)";
-             "Line Trigger (PCI6353(2) - PFI12/P2.4)"];
+ColorMap = hsv(4);
+t2.LegendLabels = ["Line Trigger (PCI6353(2) - PFI12/P2.4)";
+                   "H-Sync (MDR-R)"];
 t2.AxesLabels = ["25\mus", "10\mus", "250ns", "250ns", "250ns", "500ns", "100ns", "100ns"];
-for i = 6:10
+for i = 6:11
     if i == 6
         ax = nexttile(t2.Layout, 1);
+        Flag = true;
     elseif i == 7
         ax = nexttile(t2.Layout, 2);
+        Flag = true;
     elseif i > 7 && i < 11
-        ax = nexttile(t2.NestedLayout, i-7);
+        ax = nexttile(t2.NestedLayout1, i-7);
+        Flag = true;
     else
-        
+        ax = nexttile(t2.Layout, 4);
+        Flag = false;
     end
     hold on;
-    plot(ax, Oscope.Time(2*i+7,:), Oscope.Voltage(2*i+7,:));
-    plot(ax, Oscope.Time(2*i+8,:), Oscope.Voltage(2*i+8,:));
+    if Flag == true;
+        plot(ax, Oscope.Time(2*i+8,:), Oscope.Voltage(2*i+8,:), "Color", ColorMap(3,:));
+        plot(ax, Oscope.Time(2*i+7,:), Oscope.Voltage(2*i+7,:), "Color", ColorMap(2,:));
+        axis tight;
+        ylim([-0.52, 5.4]);
+    else
+        plot(ax, Oscope.Time(30,:), Oscope.Voltage(30,:), "Color", ColorMap(3,:));
+        axis tight;
+    end
     set(gca, "Color", [0, 0, 0], "XColor", "White", "YColor", "White");
-    axis tight;
-    ylim([-0.52, 5.4]);
     xlabel(ax, t2.AxesLabels(i-5));
 end
+nexttile(1)
+t2.Legend = legend(t2.LegendLabels, "Location", "best");
+t2.Legend.TextColor = "White";
 
 
 
