e9a8338ac7c85e798b69d35e1b8ae25380c8f08a
Lockhart <jsl5865@psu.edu>
2025-06-14 12:56:56 -0400

    Now includes 'TroubleShoot' SearchMode. Basically if you are constantly using the same file as a demo, you can predefine the path so that you don't have to continously select the filepath with uigetdir(). Very convienient

--- Functions/FileLookup.m
+++ Functions/FileLookup.m
@@ -1,31 +1,39 @@
-function Lookup = FileLookup(FileType, SearchMode)
+function Lookup = FileLookup(FileType, SearchMode, ConstantAdress)
     %% Validate file type 
         if ~ischar(FileType) && ~isstring(FileType)                                 % Ensure input is a string ('csv', 'txt', etc)
             error('File type must be a string or character array.');                %   Throw error message otherwise
         end                                                                         %   Continue
         Lookup.FileType = strcat('*.', FileType);                                   % Choose file type
     
-    %% Determine search type (Single Folder || All subfolders)
+    %% Determine search mode (Single Folder || All Subfolders || Constant TroubleShoot)
         if nargin < 2 || isempty(SearchMode)                                        % Assume no subfolders unless otherwise stated
-            SearchMode = 'single';                                                  %   Define file search based on empty argument
-        elseif ~any(strcmpi(SearchMode, {'single', 'all'}))                         %   Validate second argument
-            error("SearchMode must be 'single' or 'all'.");                         %       Throw error message
+            SearchMode = 'Single';                                                  %   Define file search based on empty argument
+        elseif ~any(strcmpi(SearchMode, {'Single', 'All', 'TroubleShoot'}))              %   Validate second argument
+            error("SearchMode must be 'Single', 'All', or 'TroubleShoot'.");             %       Throw error message
         end                                                                         %   Continue
 
     %% Select the folder
-        Lookup.FolderAddress = uigetdir('*.*','Select a folder');                   % Choose folder path location
-        if isequal(Lookup.FolderAddress, 0)                                         % Esure directory is found and user did not cancel operation
-            error('No folder selected.');                                           %   Throw error message otherwise
-        end                                                                         %   Continue
+        if strcmpi(SearchMode, 'TroubleShoot')                                      % SearchMode = Constant filepath for testing
+            if nargin < 3 || ~isfolder(ConstantAdress)                              %   Determine if three fields are provided  
+                error("For 'TroubleShoot' mode, you must provide a" + ...           %       Throw error message if filepath not defined
+                      " valid folder path as the third argument.");                 %   
+            end                                                                     %   Continue
+            Lookup.FolderAddress = ConstantAdress;                                  %   Define folder path location
+        else                                                                        %   SearchMode /= Constant filepath for testing
+            Lookup.FolderAddress = uigetdir('*.*','Select a folder');               %   Choose folder path location
+            if isequal(Lookup.FolderAddress, 0)                                     %       Esure directory is found and user did not cancel operation
+                error('No folder selected.');                                       %         Throw error message otherwise
+            end                                                                     %         Continue
+        end
 
     %% Determine file search based on SearchMode
-        if strcmpi(SearchMode, 'all')                                               % SearchMode = All files within subfolders
-            searchPattern = fullfile(Lookup.FolderAddress, '**', Lookup.FileType);  %   Find all FileType within subfolders
+        if strcmpi(SearchMode, 'All')                                               % SearchMode = All files within subfolders
+            searchPattern = fullfile(Lookup.FolderAddress, '**', Lookup.FileType);  %   Find All FileType within subfolders
         else                                                                        %   SearchMode /= All files within subfolders
-            searchPattern = fullfile(Lookup.FolderAddress, Lookup.FileType);        %   Find all Filetype within single folder
+            searchPattern = fullfile(Lookup.FolderAddress, Lookup.FileType);        %   Find All Filetype within Single folder
         end                                                                         %   continue
 
-    %% Find all FileType within defined folder
+    %% Find All FileType within defined folder
         Lookup.AllFiles = searchPattern;                                            % Create general file path
         Lookup.FolderInfo = dir(searchPattern);                                     % Identify the folder directory
         Lookup.FileCount = length(Lookup.FolderInfo);                               % Determine the number of files in this folder
