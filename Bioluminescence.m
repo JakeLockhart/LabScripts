6d04708bd741b4b3747b2c47b18f010c2b98bd8a
Lockhart <jsl5865@psu.edu>
2025-06-15 12:59:15 -0400

    Averaged data & growth rate constant implemented. Better overall setup with functions

--- Bioluminescence.m
+++ Bioluminescence.m
@@ -1,6 +1,7 @@
+%% Plot Results
 clear; clc; format short; format compact;
-addpath('C:\Workspace\LabScripts\Functions');
-Lookup = FileLookup('xlsx');
+Path = "C:\Users\jsl5865\OneDrive\Documents\UTK\Research\Microfluidics, Tissue Engineering Lab";
+Lookup = FileLookup('xlsx', 'TroubleShoot', Path);
 tic
 
 %% Read .xlxs file. (Bioluminescence Analysis - Raw Data (CAG-Lux) & Raw Data (CMV-Lux))
@@ -43,132 +44,149 @@ for i = 1:length(Sheets)
     toc
 end
 
-%% Plot Results
-% Plot Raw Data
-for i = 1:length(SheetNames)
-    figure
-    t = tiledlayout(length(Drugs), length(Concentrations), "TileSpacing", "compact");
-    title(t, ['Raw Bioluminescence Data (' SheetNames{i} ')']);
-    SheetName = SheetNames{i}; 
-    for j = 1:length(Drugs)
-        Drug = Drugs{j};
-        AllData = Data.Original.(SheetName).(Drug);
-        Time = AllData(:,1);
-        DataOnly = AllData(:,2:end);
-        Groups = length(Concentrations);
-        
-        MeanData = table2array(Data.Mean.(SheetName).(Drug));
-        MeanTime = MeanData(:,1);
-        MeanValues = MeanData(:,2:end);
-        
-        for k = 1:Groups
-            Tile = (j-1)*Groups + k;
-            nexttile(t, Tile);
-            Column = (k-1)*N + (1:N);
-            GroupData = DataOnly(:, Column);
-        
-            p1 = plot(Time, GroupData, 'Color', [0.2 0.5 0.8 0.3]);
-            hold on;
-            p2 = plot(MeanTime, MeanValues(:,k), 'k', 'LineWidth', 2);
+%% Plot Data
+PlotResults = {@Plot_CombinedAverage};
+PlotBioluminescenceResults(Data,Drugs,Concentrations, SheetNames, PlotResults)
 
-            title([Drug ' - ' Concentrations{k}], "Interpreter", "none");
+%% Functions to plot data
+function PlotBioluminescenceResults(Data, Drugs, Concentrations, SheetNames, PlotResults)
+    for i = 1:length(PlotResults)
+        feval(PlotResults{i}, Data, Drugs, Concentrations, SheetNames);
+    end
+end
+
+function Plot_RawData(Data, Drugs, Concentrations, SheetNames)
+    %% Plot Raw Data
+    for i = 1:length(SheetNames)
+        figure
+        t = tiledlayout(length(Drugs), length(Concentrations), "TileSpacing", "compact");
+        title(t, ['Raw Bioluminescence Data (' SheetNames{i} ')']);
+        SheetName = SheetNames{i}; 
+        for j = 1:length(Drugs)
+            Drug = Drugs{j};
+            AllData = Data.Original.(SheetName).(Drug);
+            Time = AllData(:,1);
+            DataOnly = AllData(:,2:end);
+            Groups = length(Concentrations);
+
+            MeanData = table2array(Data.Mean.(SheetName).(Drug));
+            MeanTime = MeanData(:,1);
+            MeanValues = MeanData(:,2:end);
+            
+            for k = 1:Groups
+                Tile = (j-1)*Groups + k;
+                nexttile(t, Tile);
+                Column = (k-1)*9 + (1:9); % assumes N=9
+                GroupData = DataOnly(:, Column);
+            
+                p1 = plot(Time, GroupData, 'Color', [0.2 0.5 0.8 0.3]);
+                hold on;
+                p2 = plot(MeanTime, MeanValues(:,k), 'k', 'LineWidth', 2);
+                title([Drug ' - ' Concentrations{k}], "Interpreter", "none");
+                xlabel('Time [Hrs]');
+                ylabel('Luminescence');
+                grid on; axis tight; hold off;
+            end
+        end
+        legend([p1(1), p2(1)], ["Raw Data", "Mean Data"], "Location", "best")
+    end
+end
+function Plot_AveragedData(Data, Drugs, Concentrations, SheetNames)
+    ColorMap = {autumn(length(Concentrations)), abyss(length(Concentrations))};
+    for i = 1:length(SheetNames)
+        figure
+        t = tiledlayout(length(Drugs), 1, "TileSpacing", "compact");
+        title(t, ['Averaged Bioluminescence Data (' SheetNames{i} ')']);
+        SheetName = SheetNames{i};
+        for j = 1:length(Drugs)
+            Drug = Drugs{j};
+            AllData = table2array(Data.Mean.(SheetName).(Drug));
+            Time = AllData(:,1);
+            DataOnly = AllData(:,2:end);
+
+            nexttile(j);
+            for k = 1:size(DataOnly, 2)
+                plot(Time, DataOnly(:,k), 'Color', ColorMap{i}(k,:), 'LineWidth', 1.5);
+                hold on;
+            end
+            hold off;
+            title(Drug, "Interpreter", "none");
             xlabel('Time [Hrs]');
             ylabel('Luminescence');
             grid on;
             axis tight;
-            hold off;
         end
+        nexttile(1)
+        legend(Concentrations, "Location", "bestoutside");
     end
-    toc
-    Lines = findobj(gca, 'Type', 'line');
-    legend([p1(1), p2(1)], ["Raw Data", "Mean Data"], "Location", "best")
 end
-
-% Plot Average
-ColorMap = {autumn(length(Concentrations)), abyss(length(Concentrations))};
-for i = 1:length(SheetNames)
+function Plot_CombinedAverage(Data, Drugs, Concentrations, SheetNames)
     figure
-    t = tiledlayout(length(Drugs), 1, "TileSpacing", "compact");
-    title(t, ['Averaged Bioluminescence Data (' SheetNames{i} ')']);
-    SheetName = SheetNames{i};
-    for j = 1:length(Drugs)
-        Drug = Drugs{j};
-        AllData = table2array(Data.Mean.(SheetName).(Drug));
-        Time = AllData(:,1);
-        DataOnly = AllData(:,2:end);
-        Groups = length(DescriptiveStatistics_List);
-
-        Tile = j;
-        nexttile(t, Tile);
-
-        Column = (1:length(Concentrations));
-        GroupData = DataOnly(:, Column);
-
-        for k = 1:size(GroupData, 2)
-            plot(Time, GroupData(:,k), 'Color', ColorMap{i}(k,:), 'LineWidth', 1.5);
-            hold on;
+    ColorMap = {autumn(length(Concentrations)), abyss(length(Concentrations))};
+    t = tiledlayout(length(Drugs), 1);
+    title(t, "Combined Average Bioluminescence");
+    ControlIdx = length(Concentrations);
+    for i = 1:length(Drugs)
+        nexttile(i); hold on;
+        Drug = Drugs{i};
+        for j = 1:length(SheetNames)
+            SheetName = SheetNames{j};
+            AllData = table2array(Data.Mean.(SheetName).(Drug));
+            Time = AllData(:,1);
+            DataOnly = AllData(:,2:end);
+            for k = 1:size(DataOnly, 2)
+                LineStyle = '-';
+                if j == 2 && k == ControlIdx
+                    LineStyle = '--';
+                end
+                plot(Time, DataOnly(:,k), 'Color', ColorMap{j}(k,:), 'LineWidth', 1.5, 'LineStyle', LineStyle);
+            end
         end
-        hold off;
-        title(Drug, "Interpreter", "none");
-        xlabel('Time [Hrs]');
-        ylabel('Luminescence');
-        grid on;
-        axis tight;
-    end
-    nexttile(1)
-    legend(Concentrations, "Location", "bestoutside");
-    toc
-end
-
-% Plot combined average
-figure
-t = tiledlayout(length(Drugs), 1, 'TileSpacing', 'compact');
-for j = 1:length(Drugs)
-    nexttile(j);
-    hold on;
-
-    SheetName1 = SheetNames{1};
-    Drug = Drugs{j};
-    AllData1 = table2array(Data.Mean.(SheetName1).(Drug));
-    Time1 = AllData1(:,1);
-    DataOnly1 = AllData1(:, 2:end);
-    GroupData1 = DataOnly1(:, 1:length(Concentrations));
-
-    for k = 1:size(GroupData1, 2)
-        plot(Time1, GroupData1(:, k), 'Color', ColorMap{1}(k,:), 'LineWidth', 1.5);
+        title(Drug, 'Interpreter', 'none');
+        xlabel('Time [Hrs]'); ylabel('Luminescence');
+        grid on; axis tight;
     end
 
-    SheetName2 = SheetNames{2};
-    AllData2 = table2array(Data.Mean.(SheetName2).(Drug));
-    Time2 = AllData2(:,1);
-    DataOnly2 = AllData2(:, 2:end);
-    GD2 = DataOnly2(:, 1:length(Concentrations));
-
-    for k = 1:size(GD2, 2)
-        plot(Time2, GD2(:, k), 'Color', ColorMap{2}(k,:), 'LineWidth', 1.5);
+    legendHandles = gobjects(1, 2*length(Concentrations));
+    legendLabels = strings(1, 2*length(Concentrations));
+    for k = 1:length(Concentrations)
+        legendHandles(k) = plot(nan, nan, '-', 'Color', ColorMap{1}(k,:), 'LineWidth', 1.5);
+        legendLabels(k) = ['CAG - ' Concentrations{k}];
+        ls = '-'; if k == ControlIdx, ls = '--'; end
+        legendHandles(k + length(Concentrations)) = plot(nan, nan, ls, 'Color', ColorMap{2}(k,:), 'LineWidth', 1.5);
+        legendLabels(k + length(Concentrations)) = ['CMV - ' Concentrations{k}];
     end
-
-    hold off;
-    title(Drug, 'Interpreter', 'none');
-    xlabel('Time [Hrs]');
-    ylabel('Luminescence');
-    grid on;
-    axis tight;
-    toc
+    legend(legendHandles, legendLabels, 'Location', 'southoutside', 'Orientation', 'horizontal', 'NumColumns', 7);
 end
-hold on;
-legendHandles = gobjects(1, 2*length(Concentrations));
-legendLabels = strings(1, 2*length(Concentrations));
-for k = 1:length(Concentrations)
-    legendHandles(k) = plot(nan, nan, '-', 'Color', ColorMap{1}(k,:), 'LineWidth', 1.5);
-    legendLabels(k) = Concentrations{k};
-    legendHandles(k+length(Concentrations)) = plot(nan, nan, '-', 'Color', ColorMap{2}(k,:), 'LineWidth', 1.5);
-    legendLabels(k+length(Concentrations)) = [Concentrations{k}];
+function Plot_InstantaneousGrowthRate(Data, Drugs, Concentrations, SheetNames)
+    for i = 1:length(SheetNames)
+        figure
+        ColorMap = {autumn(length(Concentrations)), abyss(length(Concentrations))};
+        t = tiledlayout(length(Drugs), 1, "TileSpacing", "compact");
+        title(t, ['Instantaneous Growth Rate Constants (' SheetNames{i} ')']);
+        SheetName = SheetNames{i};
+        for j = 1:length(Drugs)
+            Drug = Drugs{j};
+            AllData = table2array(Data.InstantaneousGrowthRate.(SheetName).(Drug));
+            Time = AllData(:,1);
+            DataOnly = AllData(:,2:end);
+
+            nexttile(j);
+            for k = 1:size(DataOnly, 2)
+                plot(Time, DataOnly(:,k), 'Color', ColorMap{i}(k,:), 'LineWidth', 1.5);
+                hold on;
+            end
+            hold off;
+            title(Drug, "Interpreter", "none");
+            xlabel('Time [Hrs]');
+            ylabel('Growth Rate');
+            grid on;
+            axis tight;
+        end
+        nexttile(1)
+        legend(Concentrations, "Location", "bestoutside");
+    end
 end
-legend(legendHandles, legendLabels, 'Location', 'southoutside', 'Orientation', 'horizontal', 'NumColumns', 7);
-
-
-
 
 
 %% Functions to calculate statistics
@@ -190,4 +208,46 @@ function [Time, r] = InstantaneousGrowthRate(DataArray)
     r = diff(log(Data)) ./ diff(Time);
 end
 
-%% Functions to plot
+%% Misc functions
+function Lookup = FileLookup(FileType, SearchMode, ConstantAdress)
+    %% Validate file type 
+        if ~ischar(FileType) && ~isstring(FileType)                                 % Ensure input is a string ('csv', 'txt', etc)
+            error('File type must be a string or character array.');                %   Throw error message otherwise
+        end                                                                         %   Continue
+        Lookup.FileType = strcat('*.', FileType);                                   % Choose file type
+    
+    %% Determine search mode (Single Folder || All Subfolders || Constant TroubleShoot)
+        if nargin < 2 || isempty(SearchMode)                                        % Assume no subfolders unless otherwise stated
+            SearchMode = 'Single';                                                  %   Define file search based on empty argument
+        elseif ~any(strcmpi(SearchMode, {'Single', 'All', 'TroubleShoot'}))              %   Validate second argument
+            error("SearchMode must be 'Single', 'All', or 'TroubleShoot'.");             %       Throw error message
+        end                                                                         %   Continue
+
+    %% Select the folder
+        if strcmpi(SearchMode, 'TroubleShoot')                                      % SearchMode = Constant filepath for testing
+            if nargin < 3 || ~isfolder(ConstantAdress)                              %   Determine if three fields are provided  
+                error("For 'TroubleShoot' mode, you must provide a" + ...           %       Throw error message if filepath not defined
+                      " valid folder path as the third argument.");                 %   
+            end                                                                     %   Continue
+            Lookup.FolderAddress = ConstantAdress;                                  %   Define folder path location
+        else                                                                        %   SearchMode /= Constant filepath for testing
+            Lookup.FolderAddress = uigetdir('*.*','Select a folder');               %   Choose folder path location
+            if isequal(Lookup.FolderAddress, 0)                                     %       Esure directory is found and user did not cancel operation
+                error('No folder selected.');                                       %         Throw error message otherwise
+            end                                                                     %         Continue
+        end
+
+    %% Determine file search based on SearchMode
+        if strcmpi(SearchMode, 'All')                                               % SearchMode = All files within subfolders
+            searchPattern = fullfile(Lookup.FolderAddress, '**', Lookup.FileType);  %   Find All FileType within subfolders
+        else                                                                        %   SearchMode /= All files within subfolders
+            searchPattern = fullfile(Lookup.FolderAddress, Lookup.FileType);        %   Find All Filetype within Single folder
+        end                                                                         %   continue
+
+    %% Find All FileType within defined folder
+        Lookup.AllFiles = searchPattern;                                            % Create general file path
+        Lookup.FolderInfo = dir(searchPattern);                                     % Identify the folder directory
+        Lookup.FileCount = length(Lookup.FolderInfo);                               % Determine the number of files in this folder
+        Lookup.FolderCount = length(unique({Lookup.FolderInfo.folder}));            % Determine the number of folders 
+        [~, Lookup.CurrentFolder] = fileparts(Lookup.FolderAddress);                % Collect folder information
+end
