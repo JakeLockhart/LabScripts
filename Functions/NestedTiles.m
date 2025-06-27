ba9b478dd381e38f751fc216e8492183f0f7d1a7
Lockhart <jsl5865@psu.edu>
2025-06-27 16:21:17 -0400

    Bug fixed, added parent and child layout directions.

--- Functions/NestedTiles.m
+++ Functions/NestedTiles.m
@@ -1,16 +1,17 @@
-function [FigureWindow, Main, Children] = NestedTiles(ParentLayout, ChildLayout, Direction)
+function [FigureWindow, Main, Children] = NestedTiles(ParentLayout, ChildLayout, ParentDirection, ChildDirection)
     arguments
         ParentLayout (1,1) {mustBeInteger, mustBePositive}
         ChildLayout (1,:) cell {mustBeEqualSize(ChildLayout, ParentLayout)}
-        Direction (1,1) string {mustBeMember(Direction, ["Row", "Column"])} = "Row"
+        ParentDirection (1,1) string {mustBeMember(ParentDirection, ["HorizontalLayout", "VerticalLayout"])} = "HorizontalLayout"
+        ChildDirection (1,1) string {mustBeMember(ChildDirection, ["HorizontalLayout", "VerticalLayout"])} = "VerticalLayout"
     end
 
     FigureWindow = figure;
 
-    switch Direction
-        case "Column"
+    switch ParentDirection
+        case "VerticalLayout"
             Main = tiledlayout(FigureWindow, ParentLayout, 1, 'TileSpacing', 'compact', 'Padding', 'compact');
-        case "Row"
+        case "HorizontalLayout"
             Main = tiledlayout(FigureWindow, 1, ParentLayout, 'TileSpacing', 'compact', 'Padding', 'compact');
     end
 
@@ -21,7 +22,13 @@ function [FigureWindow, Main, Children] = NestedTiles(ParentLayout, ChildLayout,
         TilePosition = ax.Position;
         delete(ax)
         Tile = uipanel(FigureWindow, "Position", TilePosition);
-        Children(i) = tiledlayout(Tile, ChildLayout{i}, 1, 'TileSpacing', 'compact');
+        
+        switch ChildDirection
+            case "VerticalLayout"
+                Children(i) = tiledlayout(Tile, ChildLayout{i}, 1, 'TileSpacing', 'compact', 'Padding', 'compact');
+            case "HorizontalLayout"
+                Children(i) = tiledlayout(Tile, 1, ChildLayout{i}, 'TileSpacing', 'compact', 'Padding', 'compact');
+        end
     end
 
 
