b7374233f0cc34306d65d39bf0b514b63db0a39f
Lockhart <jsl5865@psu.edu>
2025-07-12 16:01:21 -0400

    Validating digital pin 2 has not been shorted. (Pin still functional 7/12/25)

--- Arduino/ETLandEOM_Controller/GIGAtest/src/main.cpp
+++ Arduino/ETLandEOM_Controller/GIGAtest/src/main.cpp
@@ -1,58 +1,17 @@
 #include <Arduino.h>
-
 #define DAC0 A12
 
 volatile int counter = 0;
 
-//void myISR() {
-//    analogWrite(DAC0, 1000);
-//    digitalWrite(7, HIGH);
-//    analogWrite(DAC0, 0);
-//    digitalWrite(7,LOW);
-//}
-//
-//void setup() {
-//    Serial.begin(9600);
-//    pinMode(2, INPUT);
-//    pinMode(DAC0, OUTPUT);
-//    analogWriteResolution(12);
-//    attachInterrupt(digitalPinToInterrupt(2), myISR, RISING);
-//}
-//
-//void loop() {
-//    Serial.println(counter);
-//    delay(100);
-//}
-
-//volatile bool updateDAC = false;  // Flag to trigger DAC update
-//
-//void myISR() {
-//    updateDAC = true;  // Set flag inside ISR
-//}
-//
-//void setup() {
-//    pinMode(2, INPUT);  // Standard input mode since TTL actively drives the pin
-//    pinMode(7, OUTPUT); // Output pin
-//    analogWriteResolution(12); // Set DAC resolution to 12-bit
-//    attachInterrupt(digitalPinToInterrupt(2), myISR, RISING); // Trigger on rising edge
-//}
-//
-//void loop() {
-//    if (updateDAC) {
-//        analogWrite(A12, 1000);  // Output 1000 (out of 4095 for 12-bit DAC)
-//        delayMicroseconds(10);   // Small delay
-//        analogWrite(A12, 0);     // Reset to 0V
-//        updateDAC = false;       // Clear flag
-//    }
-//}
+int BrokenPin = 2;
 
 void setup() {
-    pinMode(7,OUTPUT);
+    pinMode(BrokenPin, OUTPUT);
 }
 void loop() {
-    digitalWrite(7,HIGH);
+    digitalWrite(BrokenPin, HIGH);
     delayMicroseconds(100);
-    digitalWrite(7,LOW);
+    digitalWrite(BrokenPin, LOW);
     delayMicroseconds(100);
 }
 
