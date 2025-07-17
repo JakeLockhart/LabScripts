ba99ec4a84995519336a6715ec5ab83f49794756
Lockhart <jsl5865@psu.edu>
2025-07-17 16:54:09 -0400

    Updated results and function. Need to address erika's comments.

--- General Drew Lab Code/Nanoplastics/MassSpecAnalysis.m
+++ General Drew Lab Code/Nanoplastics/MassSpecAnalysis.m
@@ -4,7 +4,7 @@ clear; clc;
 addpath("C:\Workspace\LabScripts\Functions");
 Lookup = FileLookup("xlsx","TroubleShoot", "C:\Users\jsl5865\Desktop\OneDrive_1_7-15-2025");
 
-%% Data
+%% Data Collection
 Variables = [ "Checked"
                  "Master"
                  "Accession"
@@ -55,10 +55,10 @@ NumericVariables = matlab.lang.makeValidName(NumericVariables);
 TextVariables = matlab.lang.makeValidName(TextVariables);
 
 for i = 1:Lookup.FileCount
-    GelBand = erase(Lookup.FolderInfo(i).name, ".xlsx");
-    TempFile = readtable(fullfile(Lookup.FolderInfo(i).folder, GelBand), "VariableNamingRule", "preserve");
+    GelBand{i} = erase(Lookup.FolderInfo(i).name, ".xlsx");
+    TempFile = readtable(fullfile(Lookup.FolderInfo(i).folder, GelBand{i}), "VariableNamingRule", "preserve");
     for j = 1:length(AllVariables)
-        GelBand_Name = matlab.lang.makeValidName(GelBand);
+        GelBand_Name = matlab.lang.makeValidName(GelBand{i});
         if ismember(AllVariables(j), NumericVariables)
             MassSpecData.(GelBand_Name).(AllVariables(j)) = table2array(TempFile(:,j));
         else
@@ -71,6 +71,35 @@ for i = 1:Lookup.FileCount
     Variables = union(Variables, "NSAF", 'stable');
 end
 
+%% User Selection
+DisplayGels = listdlg("PromptString", "Compare which gels?", "SelectionMode", "Multiple", "ListString", GelBand);
+DisplayGels = GelBand(DisplayGels);
+fprintf("Comparing the following gel bands:\n")
+for i = 1:length(DisplayGels)
+    fprintf('\t%s\n', DisplayGels{i})
+end
+DisplayGels = matlab.lang.makeValidName(DisplayGels);
+
+DisplayVariables = listdlg("PromptString", "Select Variables", "SelectionMode", "Multiple", "ListString", Variables);
+DisplayVariables = union("Description", Variables(DisplayVariables));
+fprintf("Variables chosen:\n")
+for i = 1:length(DisplayVariables)
+    fprintf('\t%s\n', DisplayVariables(i))
+end
+DisplayVariables = matlab.lang.makeValidName(DisplayVariables);
+
+SortCriteria = listdlg("PromptString", "Sort by which variable?", "SelectionMode", "single", "ListString", DisplayVariables);
+SortCriteria = DisplayVariables(SortCriteria);
+fprintf("Sorting by:\n")
+fprintf('\t%s\n', SortCriteria)
+SortCriteria = matlab.lang.makeValidName(SortCriteria);
+
+%% Output
+
+Result_1n2 = setdiff(MassSpecData.(DisplayGels{1}).Description, MassSpecData.(DisplayGels{2}).Description);
+Result_2n1 = setdiff(MassSpecData.(DisplayGels{2}).Description, MassSpecData.(DisplayGels{1}).Description);
+
+
 %% Functions
 function SAF = Calculate_SAF(MassSpecData, GelBand_Name)
     SAF = MassSpecData.(GelBand_Name).x_PSMs ./ MassSpecData.(GelBand_Name).x_AAs;
@@ -81,12 +110,4 @@ function NSAF = Calculate_NSAF(MassSpecData, GelBand_Name)
     NSAF = SAF ./ sum(SAF);
 end
 
-%% Display
-DisplayVariables = listdlg("PromptString", "Select Variables", "SelectionMode", "Multiple", "ListString", Variables);
-fprintf("Variables chosen:\n")
-for i = 1:length(DisplayVariables)
-    fprintf('\t%s\n', Variables(DisplayVariables(i)))
-end
-SortVariable = listdlg("PromptString", "Sort by which variable?", "SelectionMode", "single", "ListString", Variables(DisplayVariables));
-fprintf("Sorting by:\n")
-fprintf('\t%s\n', Variables(DisplayVariables(SortVariable)))
+y = setdiff(MassSpecData.(DisplayGels{2}).Description, MassSpecData.(DisplayGels{1}).Description)
