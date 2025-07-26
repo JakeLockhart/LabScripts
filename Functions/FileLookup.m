a82b79650a550fbac785aa7f58c9d0d8e1530b1b
Lockhart <jsl5865@psu.edu>
2025-07-26 18:35:41 -0400

    Updated to included single file selection and added the file path to the Lookup structure

--- Functions/FileLookup.m
+++ Functions/FileLookup.m
@@ -11,9 +11,10 @@ function Lookup = FileLookup(FileType, SearchMode, ConstantAddress)
     %       the file address, total number of files/subfolders, the folder information structure, and the current 
     %       folder.
     %   Any file type can be chosen; a preset list has been made which can be extended.
-    %   Currently supports multiple search modes:   Files within a single folder (SingleFolder)
+    %   Currently supports multiple search modes:   Singular file selection (SingleFile)
+    %                                               Files within a single folder (SingleFolder)
     %                                               All files within subfolders (AllSubFolders
-    %                                               One file (TroubleShoot)
+    %                                               One constant folder (TroubleShoot)
     %
     % Input: 
     %   FileType        - The file extension for the type of desired files (.csv, .txt, .jpg, etc)
@@ -25,37 +26,47 @@ function Lookup = FileLookup(FileType, SearchMode, ConstantAddress)
     %   Lookup.{FileType, FolderAddress, AllFiles, FolderInfo.{name, folder, date, bytes, isdir, datenum}, FileCount, FolderCount, CurrentFolder}
     arguments
         FileType char {mustBeMember(FileType, {'csv', 'xlsx', 'txt', 'tif', 'mdf'})};
-        SearchMode char {mustBeMember(SearchMode, {'SingleFolder', 'AllSubFolders', 'TroubleShoot'})} = 'SingleFolder';
+        SearchMode char {mustBeMember(SearchMode, {'SingleFile', 'SingleFolder', 'AllSubFolders', 'TroubleShoot'})} = 'SingleFolder';
         ConstantAddress char = ''
     end
     %% Define the file structure
-        Lookup.FileType = strcat('*.', FileType);                                   % Choose file type
+        Lookup.FileType = strcat('*.', FileType);   % Choose file type
     
     %% Select the folder
-        if strcmpi(SearchMode, 'TroubleShoot')                                      % SearchMode = Constant filepath for testing
-            if ~isfolder(ConstantAddress)                                           %   Determine if three fields are provided  
-                error("For 'TroubleShoot' mode, you must provide a" + ...           %       Throw error message if filepath not defined
-                      " valid folder path as the third argument.");                 %   
-            end                                                                     %   Continue
-            Lookup.FolderAddress = ConstantAddress;                                 %   Define folder path location
-        else                                                                        %   SearchMode /= Constant filepath for testing
-            Lookup.FolderAddress = uigetdir('*.*','Select a folder');               %   Choose folder path location
-            if isequal(Lookup.FolderAddress, 0)                                     %       Esure directory is found and user did not cancel operation
-                error('No folder selected.');                                       %         Throw error message otherwise
-            end                                                                     %         Continue
+        switch SearchMode                                                               % SearchMode = Constant filepath for testing
+            case 'TroubleShoot'                                                             % User input: Troubleshoot
+                if ~isfolder(ConstantAddress)                                                   % Validate that user defined path exists and is valid
+                    error("For 'TroubleShoot' mode, you must provide a" + ...                   % Throw error if invalid
+                          " valid folder path as the third argument.");      
+                end
+                    Lookup.FolderAddress = ConstantAddress;                                     % Set folder address to user defined path
+                    searchPattern = fullfile(Lookup.FolderAddress, Lookup.FileType);            % Create a search pattern based on folder address
+            case 'SingleFile'                                                               % User input: SingleFile
+                [FileName, FolderPath] = uigetfile(Lookup.FileType, 'Select a file');           % Prompt user to select a single file
+                if isequal(FileName, 0)                                                         % Validate file selection 
+                    error("No file selected");                                                  % Throw error if invalid
+                end
+                Lookup.FolderAddress = FolderPath;                                              % Set folder address to user defined path
+                searchPattern = fullfile(Lookup.FolderAddress, FileName);                       % Create a search pattern based on folder address
+            case 'SingleFolder'                                                             % User input: SingleFolder
+                Lookup.FolderAddress = uigetdir('*.*', 'Select a folder');                      % Prompt user to select a folder
+                if isequal(Lookup.FolderAddress, 0)                                             % Validate folder selection
+                    error('No folder selected');                                                % Throw error if invalid
+                end
+                searchPattern = fullfile(Lookup.FolderAddress, Lookup.FileType);                % Create a search pattern based on an individual folder
+            case 'AllSubFolders'                                                            % User input: Troubleshoot
+                Lookup.FolderAddress = uigetdir('*.*', 'Select a folder');                      % Prompt user to select a folder
+                if isequal(Lookup.FolderAddress, 0)                                             % Validate folder selection
+                    error('No folder selected');                                                % Throw error if invalid
+                end
+                searchPattern = fullfile(Lookup.FolderAddress, '**', Lookup.FileType);          % Create a search pattern based on all sub folders
         end
 
-    %% Determine file search based on SearchMode
-        if strcmpi(SearchMode, 'AllSubFolders')                                     % SearchMode = All files within subfolders
-            searchPattern = fullfile(Lookup.FolderAddress, '**', Lookup.FileType);  %   Find All FileType within subfolders
-        else                                                                        %   SearchMode /= All files within subfolders
-            searchPattern = fullfile(Lookup.FolderAddress, Lookup.FileType);        %   Find All Filetype within SingleFolder folder
-        end                                                                         %   continue
-
     %% Find All FileType within defined folder
-        Lookup.AllFiles = searchPattern;                                            % Create general file path
-        Lookup.FolderInfo = dir(searchPattern);                                     % Identify the folder directory
-        Lookup.FileCount = length(Lookup.FolderInfo);                               % Determine the number of files in this folder
-        Lookup.FolderCount = length(unique({Lookup.FolderInfo.folder}));            % Determine the number of folders 
-        [~, Lookup.CurrentFolder] = fileparts(Lookup.FolderAddress);                % Collect folder information
+        Lookup.AllFiles = searchPattern;                                                                    % Create general file path
+        Lookup.FolderInfo = dir(searchPattern);                                                             % Identify the folder directory
+        Lookup.FileCount = length(Lookup.FolderInfo);                                                       % Determine the number of files in this folder
+        Lookup.FolderCount = length(unique({Lookup.FolderInfo.folder}));                                    % Determine the number of folders 
+        [~, Lookup.CurrentFolder] = fileparts(Lookup.FolderAddress);                                        % Collect folder information
+        Lookup.Path = arrayfun(@(x) fullfile(x.folder, x.name), Lookup.FolderInfo, 'UniformOutput', false); % Identify the file path
 end
